63d4e409a96c9c30bde06f6250a4b6e98a1331f1
teuku <teukusalmanfarizi2003@gmail.com>
2024-09-29 00:43:20 +0700

    Restuture main

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,19 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:medgis_app/app.dart';
+import 'package:medgis_app/page/page/main_page.dart';
+import 'package:medgis_app/utils/theme/theme.dart';
 import 'package:window_manager/window_manager.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  String ip = '192.168.111.41';
+
   String pocketBasePath = '../pocketbase_0.22.20_windows_amd64/pocketbase.exe';
 
   Process pocketBaseProcess = await Process.start(
     pocketBasePath,
-    ['serve', '--http=192.168.111.41:2003'],
+    ['serve', '--http=$ip:2003'],
   );
 
   await Future.delayed(const Duration(seconds: 2));
@@ -25,7 +28,13 @@ void main() async {
     await windowManager.show();
   });
 
-  runApp(const MedGISApp());
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    theme: themeData,
+    home: MainPage(
+      database: ip,
+    ),
+  ));
 
   ProcessSignal.sigint.watch().listen((signal) {
     pocketBaseProcess.kill();
