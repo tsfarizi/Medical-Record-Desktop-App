ee5d1f556bf21e676c04123590512a751449b13b
teuku <teukusalmanfarizi2003@gmail.com>
2024-10-02 01:10:59 +0700

    Add localhost database and server id logic

--- lib/view/settings/view/settings_view.dart
+++ lib/view/settings/view/settings_view.dart
@@ -100,7 +100,7 @@ class _SettingsViewState extends State<SettingsView> {
                                           child: TextFormField(
                                             controller: _serverIdController,
                                             decoration: const InputDecoration(
-                                              hintText: 'Enter server id',
+                                              hintText: 'Input',
                                             ),
                                           ),
                                         ),
